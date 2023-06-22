53e33f56e60290c2db016758d45ab6c190fa42b7
freecree <bodya20126@gmail.com>
2023-06-22 10:01:54 +0300

    init: configure webpack with react and typescript

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'none' : 'source-map',
+    watch: !isProduction,
+    entry: ['./src/index.tsx', './src/sass/style.scss'],
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'script.js',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', 'jsx', 'js']
+    },
+
+    module: {
+      rules: [
+        // {
+        //   test: /\.js$/,
+        //   exclude: /node_modules/,
+        //   use: {
+        //     loader: 'babel-loader',
+        //     options: {
+        //     presets: ['@babel/preset-env']
+        //     }
+        //   }
+        // },
+        {
+          test: /\.tsx?$/,
+          use: {
+            loader: 'babel-loader',
+          },
+          include: [path.join(__dirname, 'src')],
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader', 'css-loader', 'sass-loader' 
+          ]
+        }, {
+          test: /\.(png|svg|jpe?g|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+            }
+          ]
+        }, {
+          test: /\.html$/,
+          loader: 'html-loader',
+        },
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      })
+    ]
+
+    // plugins: [
+    //   new CleanWebpackPlugin(),
+    //   new HtmlWebpackPlugin({
+    //     template: 'index.html'
+    //   }),
+    //   new MiniCssExtractPlugin({
+    //     filename: 'style.css'
+    //   })
+    // ]
+  }
+
+  return config;
+}
